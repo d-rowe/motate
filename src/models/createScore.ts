fa577b826eb83b6ffbeaf2687cc54653b21f8d59
Daniel Rowe <danielrowetech@gmail.com>
2021-09-18 21:59:32 -0700

    Add notes for filling score width, cleaning

--- src/models/createScore.ts
+++ src/models/createScore.ts
@@ -13,7 +13,7 @@ import type {
 
 // Width factor (min renderable width = 1)
 const VOICE_WIDTH_FACTOR = 2.2;
-const DEFAULT_MAX_WIDTH = 500;
+const DEFAULT_MAX_WIDTH = 700;
 
 type ProcessedSystemMeasure = {
     measures: MeasureModel[];
@@ -50,11 +50,11 @@ export default function createScore(
     function getSystemMeasureConfig(): SystemMeasureConfig {
         const initSystemMeasures: SystemMeasureConfig = [];
         return staves.reduce((systemMeasures, stave) => {
-            stave.measures?.forEach((measure, measureIndex) => {
+            stave.measures?.forEach((measureConfig, measureIndex) => {
                 const isFirstMeasure = measureIndex === 0;
                 systemMeasures[measureIndex] = systemMeasures[measureIndex] || [];
                 systemMeasures[measureIndex].push({
-                    ...measure,
+                    ...measureConfig,
                     clef: stave.clef,
                     hasBegBarline: !isFirstMeasure,
                     showClef: isFirstMeasure,
@@ -71,7 +71,7 @@ export default function createScore(
             measures,
             voices,
             noteStartX,
-        } = processSystemMeasures(systemMeasure);
+        } = getProcessedSystemMeasures(systemMeasure);
 
         const minVoiceWidth = formatter.preCalculateMinTotalWidth(voices);
         const voiceWidth = minVoiceWidth * VOICE_WIDTH_FACTOR;
@@ -82,13 +82,22 @@ export default function createScore(
             // we have to start a new one
             currentSystemWidth = 0;
             currentSystemIndex++;
+            // TODO: we'll have to so some re-formatting
+            //       on the previous system to stretch
+            //       across entire available width
         } else {
             currentSystemWidth += width;
         }
 
         // update measures to newly calculated width
         measures.forEach(m => m.setWidth(width));
-        // format cross-stave voices (align across x axis)
+        /**
+         * format cross-stave voices (align across x axis)
+         *
+         * once we have post-processing logic (notes below), we should
+         * try to avoid formatting until we can we can do one batch
+         * format across the entire score
+         */
         formatter.format(voices, voiceWidth);
 
         // Assure system entry exists
@@ -111,17 +120,14 @@ export default function createScore(
     }
 }
 
-function processSystemMeasures(systemMeasure: MeasureConfig[]): ProcessedSystemMeasure {
+function getProcessedSystemMeasures(systemMeasure: MeasureConfig[]): ProcessedSystemMeasure {
     let noteStartX = 0;
     const voices: VexVoice[] = [];
     const measures = systemMeasure.map(staveMeasure => {
         // TODO: cache measure model by measure/stave indexes
         //       this will help with component memoization/perf in future
         const measure = new MeasureModel(staveMeasure);
-        const currentNoteStartX = measure.stave.getNoteStartX();
-        if (currentNoteStartX > noteStartX) {
-            noteStartX = currentNoteStartX;
-        }
+        noteStartX = Math.max(measure.stave.getNoteStartX(), noteStartX);
         voices.push(measure.voice);
         return measure;
     });
